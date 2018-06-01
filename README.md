297804a7005a91cc9a77cfa56a68f6a059401b1e
Michael Angerman <stormasm@gmail.com>
2018-06-01 07:07:50 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# checkbox
+
+more info...
---
+++ v1/src/index.js
@@ -0,0 +1,10 @@
+import 'babel-polyfill'
+
+import React from 'react';
+import { render } from 'react-dom';
+import Demo from './demo';
+
+const rootElement = document.querySelector('#root');
+if (rootElement) {
+  render(<Demo />, rootElement);
+}
