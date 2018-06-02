b523469a0cbcf46046b2c8742eda5e34bc0db05b
Michael Angerman <stormasm@gmail.com>
2018-06-01 20:19:26 -0700

    not yet working, but printing the state so far...

--- v3/src/reducers/checkbox.js
+++ v3/src/reducers/checkbox.js
@@ -1,8 +1,39 @@
 import { TOGGLE_CHECKBOX } from "../actions";
 
-export const checkbox = (state = {}, action) => {
+const initialState = {
+  key1: [],
+  key2: []
+}
+
+export const checkbox = (state = initialState.key1, action) => {
   switch (action.type) {
     case TOGGLE_CHECKBOX:
+
+    const value = action.name;
+
+    // const { checked } = state;
+    const currentIndex = state.indexOf(value);
+    const newChecked = [...state];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    console.log('newChecked', newChecked);
+
+/*
+    Now go ahead and set the state...
+
+    this.setState({
+      checked: newChecked,
+    });
+*/
+
+
+
+
       console.log("Reducer checkbox ", action.name);
       return state;
     default:
