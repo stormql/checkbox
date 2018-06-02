fbf908678e39f75d786ebcd36852f9f001b4c71c
Michael Angerman <stormasm@gmail.com>
2018-06-01 21:09:11 -0700

    It is working !!

--- v3/src/reducers/index.js
+++ v3/src/reducers/index.js
@@ -2,8 +2,9 @@ import { combineReducers } from "redux";
 import { checkbox } from "./checkbox";
 
 export const getStateKey = state => {
-  console.log(state);
-  return [0,1,3];
+  console.log(state.checkbox);
+  // return [0,1,3];
+  return state.checkbox;
 }
 
 const rootReducer = combineReducers({
